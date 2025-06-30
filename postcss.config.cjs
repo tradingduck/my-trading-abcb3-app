028ad1ff833a0129be29a8529b3f70f4a401ef88
tradingduck <leisurecom@naver.com>
2025-07-01 08:11:58 +0900

    Update postcss.config.cjs

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,12 +1,6 @@
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
   },
-  plugins: [],
-  // important: true,
-  // safelist: [], 
 };
